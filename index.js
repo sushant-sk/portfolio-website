6528eff00887d27fef7b8e27df00b7f414bc5a77
sushant-sk <97774364+sushant-sk@users.noreply.github.com>
2023-07-10 00:58:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// ANOTHER METHOD TO CREATE HOVER FUNCTIONS
+
+// document.getElementById("hover").addEventListener("mouseover", function() {
+//     document.getElementById("hover").style.color = "#9AC5F4";
+// });
+
+// document.getElementById("hover").addEventListener("mouseout", function() {
+//     document.getElementById("hover").style.color = "black";
+// });
+
+function nav_hover(k){
+    k.style.color="#0d6efd";
+}
+
+function hover_leave(k){
+    k.style.color="black";
+}
+
+function icon_hover(k){
+    k.style.color="orange";
+}
+
+function icon_hover_out(k){
+    k.style.color="black";
+}
+
+function C_hover_in(k){
+    k.style.color="orange";
+}
+
